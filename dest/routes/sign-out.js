70c9a4174e393874dad94d21877d6cfa96fa7ec7
Uzukwu, John Paul <52359007+uzukwujp@users.noreply.github.com>
2022-08-21 16:56:02 +0100

    Delete dest directory

--- dest/routes/sign-out.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const common_1 = require("@jaypeeblogs/common");
-const sign_out_1 = require("../controllers/sign-out");
-const signOutRouter = (0, express_1.Router)();
-signOutRouter
-    .post('/signout', [common_1.currentUser, common_1.RequireAuth, sign_out_1.signOut]);
-exports.default = signOutRouter;
